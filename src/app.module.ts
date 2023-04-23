4a5131fe122f07da708ed5393c0139c4e7cbe545
Tran Xuan Son <tranxuanson2013@gmail.com>
2023-04-24 00:02:31 +0700

    server-sent event and some small configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,20 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SseModule } from './sse/sse.module';
 import { CacheModule } from '@nestjs/cache-manager';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        MONGO_URI: Joi.string().required(),
+      }),
+    }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
