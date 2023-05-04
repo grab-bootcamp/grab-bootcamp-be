142349495518d62839d3330d1c36eb73ee3ae176
Van Luc-Tu (denden) <luctuvan2001@gmail.com>
2023-05-04 16:22:59 +0700

    fetch data from API

--- src/livedata/cronjobs/cronjobs.service.ts
+++ src/livedata/cronjobs/cronjobs.service.ts
@@ -1,17 +1,42 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { HttpService } from '@nestjs/axios';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression, Timeout } from '@nestjs/schedule';
+import { response } from 'express';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable()
 export class CronjobsService {
+  constructor(private readonly httpService: HttpService) {}
+  private readonly logger = new Logger(CronjobsService.name);
+  // private readonly API_keys=['30f7f09560144876a51105253230305'];
 
- private readonly logger = new Logger(CronjobsService.name);
 
-//  * * * * * = min h day month day(of week)
-// * * * * * = once per minute
-  @Cron('* * * * *')
-  handleCron() {
-    this.logger.debug('Call every 5 second');
-    console.log('Call every 5 second');
+  // fetch weather API
+  fetchLiveData(keys,lat,lng){
+    const url=`http://api.weatherapi.com/v1/current.json?key= ${keys} &q= ${lng}, ${lat}`;
+    const response = this.httpService
+    .get(url)
+    .pipe(
+      map((response) => response.data),
+      catchError((e) => {
+        throw new HttpException(e.response.data, e.response.status);
+      }),
+     ); 
+   return response;
+  }
+
+ 
+  @Cron(CronExpression.EVERY_MINUTE)
+   // push to MySQL database
+  pushToDatabase(){
+    this.fetchLiveData('30f7f09560144876a51105253230305','-6.8','41.88').subscribe(val =>{
+      console.log(val)
+    })
+  }
+   
+  @Timeout(50000)
+  handleTimeout() {
+  this.logger.debug('Timeout after 5 minutes');
   }
 
 }
--- src/livedata/livedata/livedata.module.ts
+++ src/livedata/livedata/livedata.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CronjobsService } from '../cronjobs/cronjobs.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+    imports: [HttpModule],
     controllers: [],
     providers: [CronjobsService],
     exports: [CronjobsService],
