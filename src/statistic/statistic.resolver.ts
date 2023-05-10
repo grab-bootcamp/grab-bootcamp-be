d047bb3fe5f3461737d6efa9734bb46d5a4b5262
Tran Xuan Son <tranxuanson2013@gmail.com>
2023-05-10 20:00:53 +0700

    add cursor pagination

--- src/statistic/statistic.resolver.ts
+++ src/statistic/statistic.resolver.ts
@@ -7,7 +7,13 @@ export class StatisticResolver {
   constructor(private readonly statisticService: StatisticService) { }
 
   @Query(() => [Statistic], { name: 'statistic' })
-  findAll(@Args('forestId') mForestId: number, @Args('fromDate', { type: () => GraphQLISODateTime }) fromDate: Date, @Args('toDate', { type: () => GraphQLISODateTime }) toDate: Date) {
-    return this.statisticService.getStatistic(mForestId, new Date(fromDate), new Date(toDate));
+  findAll(
+    @Args('forestId') mForestId: number,
+    @Args('fromDate', { type: () => GraphQLISODateTime }) fromDate: Date,
+    @Args('toDate', { type: () => GraphQLISODateTime }) toDate: Date,
+    @Args('cursor', { nullable: true, defaultValue: null }) cursor: number,
+    @Args('size', { defaultValue: 10 }) size: number,
+  ) {
+    return this.statisticService.getStatistic(mForestId, fromDate, toDate, cursor, size);
   }
 }
